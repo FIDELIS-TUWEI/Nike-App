f01f626f0d127d74a6bee8592f6a16684d557fff
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-11 14:07:26 +0300

    Nav component imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Nav from "./components/Nav";
 import { Hero, PopularProducts, SuperQuality,
   Services, SpecialOffer, CustomerReviews,
   Subscribe, Footer
@@ -5,7 +6,7 @@ import { Hero, PopularProducts, SuperQuality,
 
 const App = () => (
   <main className="relative">
-    {/*<Nav/>*/}
+    <Nav/>
 
     <section className="xl:padding-1 wide:padding-r padding:b">
       <Hero/>
