092e242ede98015cb9ad4dd6daf37e558872ebf9
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-12 11:29:44 +0300

    additional button props/ conditional styles render added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,17 @@
 import PropTypes from "prop-types";
 
-const Button = ({ label, iconURL }) => {
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor, fullWidth }) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+    <button 
+      className={
+        `flex justify-center items-center gap-2 
+        px-7 py-4 border font-montserrat text-lg leading-none
+        ${
+          backgroundColor
+            ? `${backgroundColor} ${textColor} ${borderColor}`
+            : "bg-coral-red text-white border-coral-red"
+        } rounded-full ${fullWidth && "w-full"}`}
+    >
         {label}
 
         {iconURL && 
@@ -18,7 +27,11 @@ const Button = ({ label, iconURL }) => {
 
 Button.propTypes = {
     label: PropTypes.string,
-    iconURL: PropTypes.string
+    iconURL: PropTypes.string,
+    backgroundColor: PropTypes.string,
+    textColor: PropTypes.string,
+    borderColor: PropTypes.string,
+    fullWidth: PropTypes.number
 }
 
 export default Button;
